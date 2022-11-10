4320e535c13d2f105848f2cebdff118a736982cb
Luke Robinson <asdluke1234@gmail.com>
2022-11-10 21:45:56 +0100

    changed footer logo name

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { AiFillInstagram,AiOutlineTwitter } from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 Style Bungolo All rights reserved</p>
+      <p>2022 Style Bungalo All rights reserved</p>
       <p className='icons'>
         <AiFillInstagram />
         <AiOutlineTwitter />
